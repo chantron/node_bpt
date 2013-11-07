6cf622c527f7ed2e926decd38f343a26b86c832f
Chandler Blum <chandler@chanbook.local>
2013-11-07 12:00:02 -0800

    Initial commit

---
+++ test/bptAPI.test.js
@@ -0,0 +1,47 @@
+'user strict';
+
+var assert = require('assert'),
+	http = require('http'),
+	bpt = require('../lib/bptAPI'),
+	util = require('util');
+
+
+suite('bpt', function() {
+
+	this.timeout(0);
+
+	var devID = 'bnp74TYgNW';
+	var client = 'chandler';
+	var eventID = 153529;
+	var dateID = 741572;
+
+	test('Pull all Events', function(done) {
+		
+		var eventsList = bpt.getEvents( devID, client, function(events) {
+
+				events();
+		});
+
+
+		done();
+		
+	});
+
+	test('Pull all Dates', function(done) {
+
+		var dates = bpt.getDates(devID, eventID);
+		console.log("Dates: " + dates.list);
+		done();
+
+	});
+
+	test('Pull all Prices', function(done) {
+
+		var prices = bpt.getPrices(devID, dateID);
+		console.log("Prices: " + prices.list);
+		done();
+
+	});
+
+
+});
