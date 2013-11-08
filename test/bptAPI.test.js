f933abbdf3068e4da2a6a2e3fb31856fbe4b4961
Chandler <chandlerblum@gmail.com>
2013-11-08 15:19:56 -0800

    event list is now functional. returns a js object.

--- test/bptAPI.test.js
+++ test/bptAPI.test.js
@@ -1,30 +1,42 @@
 'user strict';
 
 var assert = require('assert'),
-	http = require('http'),
 	bpt = require('../lib/bptAPI'),
 	util = require('util');
 
+require('longjohn')
 
 suite('bpt', function() {
 
 	this.timeout(0);
 
-	var devID = 'bnp74TYgNW';
-	var client = 'chandler';
-	var eventID = 153529;
-	var dateID = 741572;
+	var bptOpts = {
+		devID: 'bnp74TYgNW', // This is required for all calls.
+		client: 'chandler',
+		//eventID: 153529,
+		dateID: 741572
+	}
+
 
 	test('Pull all Events', function(done) {
 
-		var eventsList = bpt.getEvents(devID, client, function(events) {
+		var eventList = bpt.getEvents(bptOpts, function(events) {
 
-				console.log(events);
+				events = JSON.parse(events);
+
+				var events = events['document']['event'];
+
+				console.log("\nTotal events: " + events.length);
+				console.log("--------------");
+				for ( var i = 0; i < events.length; i++ ) {
+					console.log(String(events[i]['title']));
+					console.log(String(events[i]['description']));
+					console.log("--------------");
+				}
+
 				done();
 
 		});
-
-
 
 	});
 
